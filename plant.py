912d1be256d8f090abdd81dea6f44c8a11b10440
Bioinformatics <biosinfox@gmail.com>
2018-09-08 17:45:11 +0200

    Add more methods

--- plant.py
+++ plant.py
@@ -4,22 +4,62 @@ class Plant:
 
     leaves_count=2
 
-    def __init__(self,name,variety):
+    def __init__(self,name,variety,color):
         self.name=name
         self.variety=variety
+        self.color=color
         #Increment the number of plants every time when a new instance of plant is created.
         Plant.plants+=1 
 
-    def plantData(self):
-        return '{} {}'.format(self.name,self.variety)
+    def data(self):
+        return '{} {} {}'.format(self.name,self.variety,self.color)
+    @classmethod
+    def from_string(cls,plant_str):
+        name,variety,color=plant_str.split('-')
+        return cls(name,variety,color)
 
 print(Plant.plants)
-orange=Plant('Orange','Green Variety')
+orange=Plant('Orange','Green Variety','Orange')
 print(Plant.plants)
-banana=Plant('Matooke','Muvubo')
+banana=Plant('Matooke','Muvubo','Green')
 print(Plant.plants)
-print(banana.plantData())
-print(orange.plantData())
+print(banana.data())
+print(orange.data())
 
 print(banana.__dict__)
 print(Plant.__dict__)
+
+plant_a='Apple-Green-Green'
+print(Plant.plants)
+plant_p='Pear-Green-Green'
+print(Plant.plants)
+plant_g='Grapes-Purple-Purple'
+print(Plant.plants)
+
+name,variety,color=plant_a.split('-')
+apple=Plant(name,variety,color)
+name,variety,color=plant_p.split('-')
+pear=Plant(name,variety,color)
+name,variety,color=plant_g.split('-')
+grapes=Plant(name,variety,color)
+
+print(apple.data())
+print(pear.data())
+print(grapes.data())
+
+plant_str_1='Melon-Yellow-Yellow'
+plantB=Plant.from_string(plant_str_1)
+print(plantB.data())
+
+
+
+class Apple(Plant):
+    def __init__(self,name,variety,color,leaves):
+        super().__init__(name,variety,color)
+        self.leaves=leaves
+
+
+apple1=Apple('Apple Pink','Pink Variety','Pink Color')
+print(apple1.data())
+
+
